1689410a394397df959093299fb411be3756b77e
Your Name <you@example.com>
2024-05-25 12:52:44 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// let person = {
+//     name: 'Abdulaziz',
+//     age: 16,
+// }
+// for (let key in person) {
+//     console.log(key + ":" + person[key]);
+// }
+
+
+
+
+let person = {
+    name: 'Abdulaziz',
+    age: 16,
+}
+    for (let key in person) {
+        console.log(person[key]);
+    }
